c5ec045cdcdcb6d39c57582076ec83a7548e3ea1
Shaju <shajupainkulam@gmail.com>
2021-12-18 22:45:33 +0530

    Port added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 8081
+const port = process.env.PORT || 3000
 app.use(express.json());
 
 const {MongoClient} = require('mongodb');
